034efc32934ee4ab9c0bf57c91657bc1a160caae
Kerry Wang <kerrywang369@gmail.com>
2025-01-15 21:26:03 -0800

    setup protobufs

---
+++ build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    protobuf_codegen::Codegen::new()
+        // All inputs and imports from the inputs must reside in `includes` directories.
+        .includes(&["src/protos"])
+        // Inputs must reside in some of include paths.
+        .input("src/protos/Message.proto")
+        .input("src/protos/RequestPayload.proto")
+        .input("src/protos/ResponsePayload.proto")
+        // Specify output directory relative to Cargo output directory.
+        .cargo_out_dir("protos")
+        .run_from_script();
+}
