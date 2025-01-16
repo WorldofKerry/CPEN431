722097574f2d0291ef45f218ebce28000226f8f3
Kerry Wang <kerrywang369@gmail.com>
2025-01-15 23:07:23 -0800

    cleanup to mimic prints

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use cpen431::{application::{random_message_id, ReqPayload, Request, Response}, p
 use protobuf::Message;
 
 fn get_secret_code(server_ip: Ipv4Addr, student_id: u32, port: u16) -> Option<Vec<u8>> {
-    let retries = 3;
-    let default_timeout = std::time::Duration::from_millis(100);
+    let retries = 10;
+    let default_timeout = std::time::Duration::from_millis(10);
 
     let message_id = random_message_id(port);
     let request = Msg::from_student_id(message_id, student_id).write_to_bytes().unwrap();
@@ -12,8 +12,8 @@ fn get_secret_code(server_ip: Ipv4Addr, student_id: u32, port: u16) -> Option<Ve
     let socket = std::net::UdpSocket::bind("0.0.0.0:34254").unwrap();
     socket.set_read_timeout(Some(default_timeout)).unwrap();
 
+    println!("Request: {}", hex::encode(&request));
     for retry in 0..=retries {
-        println!("{:02X?}", request);
         socket.send_to(&request, (server_ip, port)).unwrap();
 
         let mut response_data = [0; 1024];
@@ -22,13 +22,14 @@ fn get_secret_code(server_ip: Ipv4Addr, student_id: u32, port: u16) -> Option<Ve
                 let response_data = &response_data[..size];
                 let response = Msg::parse_response(response_data);
                 if response.messageID == message_id {
-                    return Some(response.response().secretKey);
+                    let secret_key = response.response().secretKey;
+                    return Some(secret_key);
                 } else {
-                    dbg!(response.to_string());
+                    println!("Invalid message ID");
                 }
             }
             Err(_) => {
-                dbg!("timeout");
+                println!("Timeout")
             }
         }
         socket.set_read_timeout(Some(default_timeout * (1 << retry))).unwrap();
@@ -37,6 +38,9 @@ fn get_secret_code(server_ip: Ipv4Addr, student_id: u32, port: u16) -> Option<Ve
 }
 
 fn main() {
-    let secret_code = get_secret_code(Ipv4Addr::from_str("52.27.39.26").unwrap(), 1381632, 43102);
-    dbg!(secret_code);
+    let student_id = 1381632;
+    let secret_code = get_secret_code(Ipv4Addr::from_str("52.27.39.26").unwrap(), student_id, 43102).unwrap();
+    println!("Student ID: {}", student_id);
+    println!("Secret Code Length: {}", secret_code.len());
+    println!("Secret Code: {}", hex::encode(&secret_code));
 }
