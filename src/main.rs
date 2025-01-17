d29798506cd3fd91851cf18f319513cc4cead98b
Kerry Wang <kerrywang369@gmail.com>
2025-01-16 22:50:32 -0800

    make server struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, net::Ipv4Addr};
+use std::{collections::HashMap, io, net::Ipv4Addr};
 use clap::Parser;
 use cpen431::{application::{random_message_id, Command, Deserialize, Request, Response, Serialize}, protocol::Msg};
 use protobuf::Message;
@@ -16,41 +16,56 @@ struct Cli {
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let cli = Cli::parse();
-    run_server(cli.ip, cli.port).await
+    Server::new(cli.ip, cli.port).run().await
 }
 
-fn handle_request(request: Request) -> Response {
-    match request.command {
-        Command::IsAlive => Response { 
-            err_code: 0,
-            ..Default::default()
-        },
-        _ => {
-            eprintln!("Unsupported command: {:?}", request.command);
-            Response {
-                err_code: 1,
+struct Server {
+    ip: Ipv4Addr,
+    port: u16,
+    kv_data: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+impl Server {
+    pub fn new(ip: Ipv4Addr, port: u16) -> Self {
+        Server {
+            ip,
+            port,
+            kv_data: HashMap::new(),
+        }
+    }
+
+    fn handle_request(&self, request: Request) -> Response {
+        match request.command {
+            Command::IsAlive => Response { 
+                err_code: 0,
                 ..Default::default()
+            },
+            _ => {
+                eprintln!("Unsupported command: {:?}", request.command);
+                Response {
+                    err_code: 1,
+                    ..Default::default()
+                }
             }
         }
     }
-}
 
-async fn run_server(ip: Ipv4Addr, port: u16) -> io::Result<()> {    
-    let sock = UdpSocket::bind((ip, port)).await?;
-    println!("Trying using debugger more instead of println");
-    println!("Server listening on {}:{}", sock.local_addr().unwrap().ip(), sock.local_addr().unwrap().port());
-    let mut buf = [0; 1024];
-    loop {
-        let (len, addr) = sock.recv_from(&mut buf).await?;
-        println!("{:?} bytes received from {:?}", len, addr);
+    pub async fn run(&self) -> io::Result<()> {
+        let sock = UdpSocket::bind((self.ip, self.port)).await?;
+        println!("Server listening on {}:{}", sock.local_addr().unwrap().ip(), sock.local_addr().unwrap().port());
+        let mut buf = [0; 1024];
+        loop {
+            let (len, addr) = sock.recv_from(&mut buf).await?;
+            println!("{:?} bytes received from {:?}", len, addr);
 
-        let req_msg = Msg::from_bytes(&buf[..len]);
-        let request = req_msg.payload();
-        let message_id = req_msg.message_id();
-        println!("{:?}", request);
+            let req_msg = Msg::from_bytes(&buf[..len]);
+            let request = req_msg.payload();
+            let message_id = req_msg.message_id();
+            println!("{:?}", request);
 
-        let response = handle_request(request);
-        let rsp_msg = response.to_bytes(message_id);
-        sock.send_to(&rsp_msg, addr).await?;
+            let response = self.handle_request(request);
+            let rsp_msg = response.to_bytes(message_id);
+            sock.send_to(&rsp_msg, addr).await?;
+        }
     }
 }
